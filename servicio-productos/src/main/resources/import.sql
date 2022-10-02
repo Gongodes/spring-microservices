455ad3bcc4a995cc4102036d5aa95771cbfc4864
Gonzalo Godoy Espinosa <gonzalo.godoy.esp@gmail.com>
2022-10-02 17:52:49 -0300

    added import.sql

---
+++ servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into productos (nombre, precio, create_at) values ('Tobasco Sauce', 2000, NOW());
+insert into productos (nombre, precio, create_at) values ('Chicken - Soup Base', 3000, NOW());
+insert into productos (nombre, precio, create_at) values ('Chutney Sauce - Mango', 23423, NOW());
+insert into productos (nombre, precio, create_at) values ('Peach - Halves', 23442, NOW());
+insert into productos (nombre, precio, create_at) values ('Coffee - Dark Roast', 23423, NOW());
+insert into productos (nombre, precio, create_at) values ('Cheese - Parmesan Grated', 2344, NOW());
+insert into productos (nombre, precio, create_at) values ('Bread - Raisin Walnut Pull', 23442, NOW());
+insert into productos (nombre, precio, create_at) values ('Nut - Chestnuts, Whole', 45646, NOW());
+insert into productos (nombre, precio, create_at) values ('Vodka - Moskovskaya', 678678, NOW());
